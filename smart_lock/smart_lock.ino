a7faeb2e35f76070634f1231bcd7ea302404e290
MKabMan <60070060@kmitl.ac.th>
2018-04-23 00:50:06 +0700

    add comment

--- smart_lock/smart_lock.ino
+++ smart_lock/smart_lock.ino
@@ -8,14 +8,14 @@
 #define FIREBASE_AUTH   "0FN1SIlY3CtL3F4nJfl38hQrKjYGyRZ1v234eRYM"
 
 
-int LED = D1;
+int LED = D1;   //Setting
 int Voice = D3;
 int i;
 
 void setup() {
   Serial.begin(250000);
   
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD); //begin connect WIFI
   Serial.print("connecting");
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
@@ -25,21 +25,21 @@ void setup() {
   Serial.print("connected: ");
   Serial.println(WiFi.localIP());
   
-  pinMode(LED, OUTPUT);
-  pinMode(Voice, OUTPUT);
-  digitalWrite(Voice, HIGH);
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-  Firebase.setInt("status", 0); 
+  pinMode(LED, OUTPUT); //Set LED PinMode
+  pinMode(Voice, OUTPUT); //Set Voice PinMode
+  digitalWrite(Voice, HIGH); //Set voice default
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH); //Connect to firebase
+  Firebase.setInt("status", 0); //Set default status
 }
 void loop() {
-  lighthigh();
-  lightlow();
+  lighthigh(); //Unlock function
+  lightlow(); //Lock function
 }
 void lighthigh(){
   delay(100);
-  if (Firebase.getInt("status") != 0){
-    digitalWrite(LED, 1);
-    for (i = 0; i <= 10; i = i+1){
+  if (Firebase.getInt("status") != 0){ //Recieve status from fireabse
+    digitalWrite(LED, 1); //Unlock
+    for (i = 0; i <= 10; i = i+1){ //Delay with Alert while its Unlock
       digitalWrite(Voice, 0);
       delay(1000);
       digitalWrite(Voice, 1);
@@ -50,8 +50,8 @@ void lighthigh(){
 
 void lightlow(){
   delay(100);
-  if (Firebase.getInt("status") != 0){
-    Firebase.setInt("status", 0);
-    digitalWrite(LED, 0);
+  if (Firebase.getInt("status") != 0){ //Auto Lock after delay
+    Firebase.setInt("status", 0); //Set status to firebase
+    digitalWrite(LED, 0); //Lock
   }
 }
